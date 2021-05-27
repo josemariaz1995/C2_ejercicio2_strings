f58c0bb4fd9b05b32f9185397ae5cd00714fea3d
Jose Maria Zamora Perez <jzamora@biada.net>
2021-05-27 10:16:01 +0200

    Correccion

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+/* correccion
+
+1. extraer las palabra
+ conts palabras = fraseSnake.split('_')
+
+2.recorrer el grupo de palbras
+ let palabrasModificadas = palabras[0];
+
++ cojera la pirmera palabra de ala array y
+  despues en for iremos añadiendole la siguiente seccion
+  de array en mayusculas
+
+for(conts palabra in palabras.slice(1)){
+ const inicialMayuscula = palabra.charAt(0).toUpperCase() + palabra.slice(1)
+  palabrasModificadas += inicialMayusculas;
+}
+console.log(palabrasModificadas);
+
+3. por cada palabra menos la primeras ponemos en mayuscula */
+
+
+
 let snake = "Hola_áuenos_dias";
 function snakeToCamel() {
-    snake= snake.split(/([-_])/g);
+    snake= snake.split("_");
+    console.log(snake);
     snake= snake.map(
-        (n) => 
+        (n) =>
            n.charAt(0).toUpperCase() + n.slice(1)
     );
     snake = snake.join("");
@@ -11,11 +34,14 @@ function snakeToCamel() {
 }
 snakeToCamel();
 
+
+const fraseCamelCase = "holaBuenosDias";
+
 let camel = "holaÁuenosDias";
 function camelToSnake() {
     camel = camel.split(/(?=[A-ZÀ-Ú])/);
-    const guion = camel.join("-");
-    const barraBaja = camel.join("_");
+    const guion = camel.join("-").toLowerCase();
+    const barraBaja = camel.join("_").toLowerCase();
     console.log(guion, barraBaja);
 
 
